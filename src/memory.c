8ded2e8b420254fbecb43fcf150480bc54b8b709
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-02-14 22:27:45 +0900

    Implement memalloc and memfree in src/memory.c

--- src/memory.c
+++ src/memory.c
@@ -1,14 +1,19 @@
+#include <stdbool.h>
 #include <stddef.h>
+#include <malloc.h>
+#include <stdlib.h>
 #include "memory.h"
 
-void* memalloc(size_t num, size_t size, const char* file, int line) {
-   (void)num;
-   (void)size;
+void* memalloc(size_t num, size_t size, bool failure, const char* file, int line) {
+   void* p = calloc(num, size);
+   if( p == NULL || failure ) {
+      abort();
+   }
    (void)file;
    (void)line;
-   return NULL;
+   return p;
 }
 
 void memfree(void* p) {
-   (void)p;
+   free(p);
 }
