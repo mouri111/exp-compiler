58a0861a678087656e8ca01da907e4d762e3a89f
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-02-14 22:38:00 +0900

    Fix tests of test_memory

--- test/test_memory.c
+++ test/test_memory.c
@@ -3,55 +3,64 @@
 #include "memory.h"
 
 int test_char_one(bool failure) {
-   char* p = (char*)memalloc(32768, sizeof(char), __FILE__, __LINE__);
+   char* p = (char*)memalloc_(32768, sizeof(char));
+   int res = 0;
    for(int i = 0; i < 32768; ++i) {
-      p[i] = i*257%32768;
+      p[i] = i*257%128;
    }
    for(int i = 0; i < 32768; ++i) {
-      if( p[i] != i*257%32768 || failure ) {
-         return 1;
+      if( p[i] != i*257%128 || failure ) {
+         res = 1;
       }
    }
-   return 0;
+   memfree(p);
+   return res;
 }
 
 int test_char_many(bool failure) {
    char* p[256];
+   int res = 0;
    for(int i = 0; i < 256; ++i) {
-      p[i] = (char*)memalloc(i, sizeof(char), __FILE__, __LINE__);
+      p[i] = (char*)memalloc_(i, sizeof(char));
    }
    for(int i = 0; i < 256; ++i) {
       for(int k = 0; k < i; ++k) {
-         p[i][k] = i^k;
+         p[i][k] = (char)i^k;
       }
    }
    for(int i = 0; i < 256; ++i) {
       for(int k = 0; k < i; ++k) {
-         if( p[i][k] != (i^k) || failure ) {
-            return 1;
+         if( p[i][k] != (char)(i^k) || failure ) {
+            res = 1;
          }
       }
    }
-   return 0;
+   for(int i = 0; i < 256; ++i) {
+      memfree(p[i]);
+   }
+   return res;
 }
 
 int test_int_one(bool failure) {
-   int* p = (int*)memalloc(32768, sizeof(int), __FILE__, __LINE__);
+   int* p = (int*)memalloc_(32768, sizeof(int));
+   int res = 0;
    for(int i = 0; i < 32768; ++i) {
       p[i] = i*257%32768;
    }
    for(int i = 0; i < 32768; ++i) {
       if( p[i] != i*257%32768 || failure ) {
-         return 1;
+         res = 1;
       }
    }
-   return 0;
+   memfree(p);
+   return res;
 }
 
 int test_int_many(bool failure) {
    int* p[256];
+   int res = 0;
    for(int i = 0; i < 256; ++i) {
-      p[i] = (int*)memalloc(i, sizeof(int), __FILE__, __LINE__);
+      p[i] = (int*)memalloc_(i, sizeof(int));
    }
    for(int i = 0; i < 256; ++i) {
       for(int k = 0; k < i; ++k) {
@@ -61,15 +70,18 @@ int test_int_many(bool failure) {
    for(int i = 0; i < 256; ++i) {
       for(int k = 0; k < i; ++k) {
          if( p[i][k] != (i^k) || failure ) {
-            return 1;
+            res = 1;
          }
       }
    }
-   return 0;
+   for(int i = 0; i < 256; ++i) {
+      memfree(p[i]);
+   }
+   return res;
 }
 
 int main(int argc, char* argv[]) {
-   if( argc == 2 || ( argc == 3 && strcmp(argv[2], "failure") ) ) {
+   if( ! ( argc == 2 || ( argc == 3 && strcmp(argv[2], "failure") ) ) ) {
       return 1;
    }
    bool failure = argc == 3;
