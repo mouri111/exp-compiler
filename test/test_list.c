db82e9cb87262a6d1c88071cc07ddc9a84349aff
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-02-19 00:06:15 +0900

    Add a test in test_list.c

--- test/test_list.c
+++ test/test_list.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include "list.h"
+#include "memory.h"
+
+typedef struct IntList IntList;
+struct IntList {
+   int x;
+   List list;
+};
 
 int main() {
+   List head;
+   list_init_(head);
+   int xs[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+   for(int i = 0; i < 8; ++i) {
+      IntList *p = memalloc_(1,sizeof(IntList));
+      p->x = xs[i];
+      list_push_back_(&head, &p->list);
+   }
+
+   int i = 0;
+
+   list_for_(IntList,list,p,head) {
+      if( i >= 8 )
+         return 1;
+
+      if( p->x != xs[i] )
+         return 1;
+
+      i += 1;
+   }
+
    return 0;
 }
