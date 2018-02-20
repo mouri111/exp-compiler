de7882cd391a44eb124144006919f8aa83754cdf
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-02-20 23:24:13 +0900

    Add list_parent_

--- src/list.h
+++ src/list.h
@@ -16,6 +16,9 @@ void list_push_back(List *head, List *p);
 List* list_pop_front(List *head) __attribute__((warn_unused_result));
 List* list_pop_back(List *head) __attribute__((warn_unused_result));
 
+#define list_parent_(T,list,p)                  \
+   (T*)((size_t)(p)-offsetof(T,list))
+
 #define list_for_(T,list,p,head)                               \
    for(T *p = (T*)((size_t)((head)->next)-offsetof(T,list));   \
        &p->list != head;                                       \
