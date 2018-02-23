357e690b4297f009104c9b131892e422c93ca126
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-02-23 15:59:27 +0900

    Add some functions

--- src/string_builder.h
+++ src/string_builder.h
@@ -4,3 +4,11 @@ typedef struct StringBuilder StringBuilder;
 
 StringBuilder* create_string_builder(void);
 void delete_string_builder(StringBuilder *builder);
+
+void string_builder_push_front(StringBuilder *builder, char c);
+void string_builder_push_back(StringBuilder *builder, char c);
+
+void string_builder_pop_front(StringBuilder *builder);
+void string_builder_pop_back(StringBuilder *builder);
+
+char* string_builder_generate_cstring(StringBuilder *builder);
