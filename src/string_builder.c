2e71084bc399e3643b4eda9ef3b3022bd59b19ec
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-02-23 15:56:48 +0900

    Implement create_string_builder and delete_string_builder

--- src/string_builder.c
+++ src/string_builder.c
@@ -1,8 +1,27 @@
 #include "string_builder.h"
+#include "list.h"
+#include "memory.h"
 
 typedef struct CharList CharList;
 struct CharList {
+   List list;
+   char c;
 };
 
 struct StringBuilder {
+   List head;
 };
+
+StringBuilder* create_string_builder(void) {
+   StringBuilder *p = memalloc_(1, sizeof(StringBuilder));
+   list_init(&p->head);
+   return p;
+}
+
+void delete_string_builder(StringBuilder *builder) {
+   if( builder == NULL )
+      return;
+
+   list_free_(CharList, head, &builder->head);
+   memfree(builder);
+}
