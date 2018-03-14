9604d5a87670ac1ca5b79ad68bcc5ecbbff20633
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-03-14 17:07:37 +0900

    Add test_string_builder_pop_back

--- test/test_string_builder.c
+++ test/test_string_builder.c
@@ -48,16 +48,40 @@ int test_string_builder_pop_front(void) {
    return 0;
 }
 
-const int num_tests = 3;
+int test_string_builder_pop_back(void) {
+   StringBuilder *builder = create_string_builder();
+   const char* str = "abcdefgh";
+   int len = strlen(str);
+
+   for(int i = 0; i < len; ++i)
+      string_builder_push_back(builder, str[i]);
+
+   for(int i = 0; i < len; ++i) {
+      char c = string_builder_pop_back(builder);
+      assert_exit_( c == str[len-i-1] );
+      char *str2 = string_builder_generate_cstring(builder);
+      assert_exit_( strncmp(str, str2, len-i-1) == 0 );
+      memfree(str2);
+   }
+
+   char c = string_builder_pop_back(builder);
+   assert_exit_( c == '\0' );
+   delete_string_builder(builder);
+   return 0;
+}
+
+const int num_tests = 4;
 int(*test_functions[])(void) = {
    test_string_builder_create_delete,
    test_string_builder_generate_cstring_1,
    test_string_builder_pop_front,
+   test_string_builder_pop_back,
 };
 const char *test_names[] = {
    "create_delete",
    "generate_cstring_1",
    "pop_front",
+   "pop_back",
 };
 
 int main(int argc, char *argv[]) {
